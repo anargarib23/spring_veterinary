f15103ecff9babc7ca89967ee87f75145e189375
anargarib23 <mehrali.anar@gmail.com>
2023-02-09 16:21:18 +0400

    Modelc created: Pet

--- src/main/java/com/project/veterinary/model/Pet.java
+++ src/main/java/com/project/veterinary/model/Pet.java
@@ -1,5 +1,83 @@
 package com.project.veterinary.model;
 
-public class Pet {
+import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+enum Species {
+	DOG, CAT, BIRD, FISH, RACCOON;
+}
+
+@Entity
+@Table(name="pet")
+public class Pet {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="species")
+	private Species species;
+	
+	@Column(name="birth_date")
+	private Date birthDate;
+	
+	@Column(name="registration_date")
+	private Date registrationDate;
+	
+	@Column(name="sterilized")
+	private Boolean sterilized;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Species getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(Species species) {
+		this.species = species;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Boolean getSterilized() {
+		return sterilized;
+	}
+
+	public void setSterilized(Boolean sterilized) {
+		this.sterilized = sterilized;
+	}
+
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", species=" + species + ", birthDate=" + birthDate + ", registrationDate="
+				+ registrationDate + ", sterilized=" + sterilized + "]";
+	}
+	
+	
+	
 }
